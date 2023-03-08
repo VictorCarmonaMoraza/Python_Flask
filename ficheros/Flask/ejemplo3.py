d9c4a0207ee15477c9f7d86cb32760e45b220a1e
Victor <VictorCarmona9035@hotmail.com>
2023-03-08 20:03:39 +0100

    68-Flask - Formulario en las plantillas

--- ficheros/Flask/ejemplo3.py
+++ ficheros/Flask/ejemplo3.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/pagina1')
 def pagina1():
@@ -10,6 +11,19 @@ def pagina1():
 @app.route('/pagina2')
 def pagina2():
     return render_template('pagina2.html')
+
+
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+
+@app.route('/gracias')
+def gracias():
+    # recuperar campos del formulario
+    valor1 = request.args.get('nombre')
+    valor2 = request.args.get('apellidos')
+    return render_template('gracias.html', nombre=valor1, apellidos=valor2)
 
 
 if __name__ == '__main__':
