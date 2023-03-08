40c2bb70ed98704baa8776e42539ee3664a7acd2
Victor <VictorCarmona9035@hotmail.com>
2023-03-08 07:37:18 +0100

    64-Flask - Uso del bucle FOR en las plantillas

--- ficheros/Flask/ejemplo2.py
+++ ficheros/Flask/ejemplo2.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 def portada():
     return render_template("portada.html")
 
+
 @app.route("/hola")
 def hola():
-    diccionario ={'nombre':'Antonio','edad':'35', 'color':'verde'}
+    diccionario = {'nombre': 'Antonio', 'edad': '35', 'color': 'verde'}
     return render_template("hola.html", datos=diccionario)
+
+
+@app.route("/colores")
+def colores():
+    lista_colores = ["verde","rojo", "amarillo", "azul"]
+    return render_template("colores.html", colores = lista_colores)
 
 
 if __name__ == '__main__':
