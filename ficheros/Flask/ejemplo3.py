c74c3d16141fabbcff58e30595c814f64951275a
Victor <VictorCarmona9035@hotmail.com>
2023-03-08 21:26:25 +0100

    72-Plantillas - Solucion al ejercicio 1

--- ficheros/Flask/ejemplo3.py
+++ ficheros/Flask/ejemplo3.py
@@ -25,9 +25,28 @@ def gracias():
     valor2 = request.args.get('apellidos')
     return render_template('gracias.html', nombre=valor1, apellidos=valor2)
 
+
+@app.route('/nombre')
+def nombre():
+    return render_template('nombre.html')
+
+
+@app.route('/resultado')
+def resultado():
+    nombre = request.args.get('nombre')
+    # Comrpueba por cada letra si hay alguna minuscula y devuelve true o false
+    minuscula = any(letra.islower() for letra in nombre)
+    # Comrpueba si tiene algun numero
+    numero = any(letra.isdigit() for letra in nombre)
+    # Comrpueba si la primera letra es mayuscula
+    mayuscula = nombre[0].isupper()
+    todo = minuscula and numero and mayuscula
+    return render_template('resultado.html', todo=todo, minuscula=minuscula, mayuscula=mayuscula, numero=numero)
+
+
 @app.errorhandler(404)
 def pagina_no_encontrada(e):
-    return render_template('pagina404.html'),404
+    return render_template('pagina404.html'), 404
 
 
 if __name__ == '__main__':
