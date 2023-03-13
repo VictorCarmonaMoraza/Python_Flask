921624d0ad515d3bc0c7c1a560588f9091838a32
Victor <VictorCarmona9035@hotmail.com>
2023-03-13 20:05:48 +0100

    78-Ejercicio con FlaskForm

--- ficheros/Flask/ejemplo6.py
+++ ficheros/Flask/ejemplo6.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField,StringField
 
 
 app = Flask(__name__)
@@ -10,6 +10,7 @@ app.config['SECRET_KEY'] = 'miclavesecreta'
 
 # Creamos el formulario
 class Formulario(FlaskForm):
+    nombre = StringField('Introduce un nombre')
     boton = SubmitField("Enviar mensaje")
 
 
@@ -21,7 +22,10 @@ def mensaje():
 
     # Verficamos si el formualrios se ha enviado
     if formulario.validate_on_submit():
-        flash("Gracias por pulsar este boton")
+        #Recupero el nombre del formulario
+        nombre = formulario.nombre.data
+        #le pasamos el nombre al mensaje mediante .format(nombre)
+        flash("Gracias {} por pulsar este boton".format(nombre))
         return redirect(url_for('mensaje'))
     return render_template('mensaje.html', formulario=formulario)
 
